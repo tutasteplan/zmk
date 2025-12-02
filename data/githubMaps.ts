8cebb14d131162197e0b8e626f2215527e32c5a7
tutasteplan <esb.urbanplanner@gmail.com>
2025-12-02 21:38:24 +0400

    Update githubMaps.ts

--- data/githubMaps.ts
+++ data/githubMaps.ts
@@ -17,6 +17,7 @@ const mapsConfig = [
    { name: 'bursa ova geojson', filename: 'OVALAR.geojson' },
   { name: 'BOKA Sınırları', filename: 'BOKA.kmz' },
   { name: 'Su Tahsis Alanları', filename: 'SU TAHSİS ALANLARI (9).kmz' }, 
+  { name: 'Su Tahsis Alanları', filename: 'SU_tahsis_alanlari.kmz' }, 
   { name: 'Tüm Korunan Alanlar', filename: 'tum_korunan_alanlar.kmz' }
 ];
 
